b848b84072a9950eaef37d4a217dbe37ca970ba0
Daniel Polk <Danielpolk7@icloud.com>
2023-07-20 14:29:02 -0400

    added criteria and supporting css and js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,6 +13,31 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+submit.addEventListener("click", writePassword);
 
+function generatePassword () {
+  
+}
 
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("generate");
+
+var span = document.getElementsByClassName("close")[0];
+ 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+var slider = document.getElementById("length");
+var output = document.getElementById("slideValue");
+output.innerHTML = `Password Length: ${slider.value} characters`;
+
+slider.oninput = function() {
+  output.innerHTML = `Password Length: ${this.value} characters`;
+  return (this.value)
+}
